65d2f1bbfaa4c565eb9db21edb76ab124e3245e6
Sam Myres <samiam2013@gmail.com>
2022-02-24 16:40:24 -0600

    works great at it's very simple job

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"math/rand"
+	"net/http"
+)
 
-func main(){
-	fmt.Println("write the server here")
+const honeypotPort = ":7668"
+
+func main() {
+	http.HandleFunc("/", dumpBytes)
+	err := http.ListenAndServe(honeypotPort, nil)
+	log.Fatal("server stopped:" + err.Error())
+}
+
+func dumpBytes(w http.ResponseWriter, req *http.Request) {
+	randomBytes := make([]byte, 10_000_000)
+	w.Header().Add("Content-Type", "text/html")
+	if _, err := rand.Read(randomBytes); err != nil {
+		log.Fatal("failed generating bytes:" + err.Error())
+	}
+	if _, err := w.Write(randomBytes); err != nil {
+		log.Fatal("failed to write bytes:" + err.Error())
+	}
 }
